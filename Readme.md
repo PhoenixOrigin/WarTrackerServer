2dc92e6732306235ce7987f8a838b0be6d8fc2d3
PhoenixOrigin <phoenixorigin171@gmail.com>
2023-09-24 13:48:15 +0100

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,20 @@
+```
+CREATE TYPE TOWER AS (
+    health            numeric,
+    defense           DOUBLE PRECISION,
+    attack_low        numeric,
+    attack_high       numeric,
+    attack_speed      DOUBLE PRECISION
+);
+```
+```
+CREATE TABLE wars (
+    uuid TEXT PRIMARY KEY,
+    war_members TEXT,
+    territory_name TEXT,
+    territory_owner TEXT,
+    start BIGINT,
+    start_tower TOWER,
+    end_tower TOWER
+);
+```
---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const config = require('config');
+const app = express();
+const port = config.get('server.port');
+const host = config.get('server.host');
+
+import { Client } from 'pg';
+const client = new Client({
+    user: config.get('database.user'),
+    password: config.get('database.password'),
+    host: config.get('database.host'),
+    port: config.get('database.port'),
+    database: config.get('database.db-name')
+});
+await client.connect();
+
+app.post('/war', async (req, res) => {
+    const query = {
+        text: "INSERT INTO wars (uuid, war_members, territory_name, territory_owner, start, start_tower, end_tower) VALUES($1, $2, $3, $4, $5, ROW($6, $6, $7, $8, $9), ROW($10, $11, $12, $13, $14))",
+        values: [
+            req.header("X-Minecraft-UUID"),
+            req.body.warMembersUUID,
+            req.body.territoryName,
+            req.body.territoryOwner,
+            req.body.startTime,
+            req.body.start.health,
+            req.body.start.defense,
+            req.body.start.attack_low,
+            req.body.start.attack_high,
+            req.body.start.attack_speed,
+            req.body.end.health,
+            req.body.end.defense,
+            req.body.end.attack_low,
+            req.body.end.attack_high,
+            req.body.end.attack_speed
+        ]
+    };
+
+    const response = await client.query(query);
+    res.sendStatus(200);
+})
+const server = app.listen(port, host, (err) => {
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
+});
